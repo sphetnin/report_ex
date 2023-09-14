703dddc95e68677e60d751fd63d0ef0563644538
sphetnin <sphetnin@gmail.com>
2023-09-14 23:22:38 +0700

    dd

--- src/components/Select/SelectProject.jsx
+++ src/components/Select/SelectProject.jsx
@@ -8,8 +8,7 @@ function SelectProject({ onSelectValue }) {
   useEffect(() => {
     // ใช้ Axios ในส่วนนี้เพื่อดึงข้อมูลจาก API
     axios
-      // .get("//192.168.0.161:5555/sap_project")
-      .get("//118.173.233.20:14443/sap_api/sap_project")
+      .get("http://118.173.233.20:14443/sap_api/sap_project")
       .then((response) => {
         const data = response.data.data;
         const formatOption = data.map((res) => ({
